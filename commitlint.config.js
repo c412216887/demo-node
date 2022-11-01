f797b52674c57f6bc79741ed5bc40703035faaae
chenlu <chenlu0917boy@163.com>
2022-11-01 10:43:14 +0800

    add husky

---
+++ commitlint.config.js
@@ -0,0 +1,11 @@
+/*
+ * @LastEditors: chenlu chenlu0917boy@163.com
+ * @Date: 2022-11-01 10:36:05
+ * @LastEditTime: 2022-11-01 10:40:25
+ * @FilePath: \demo-node\commitlint.config.js
+ */
+module.exports = {
+  extends: [
+    '@commitlint/config-conventional'
+  ]
+}
